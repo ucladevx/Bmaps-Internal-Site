164f258a54fb8e20c28bde751f5c28d658206a98
howardwang15 <joy_luck_2000@hotmail.com>
2019-05-08 23:03:22 -0700

    updated title of site

--- react-boilerplate/src/index.js
+++ react-boilerplate/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './containers/home';
+import { Helmet } from 'react-helmet';
 
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -8,6 +9,10 @@ import 'react-datepicker/dist/react-datepicker.css';
 
 const root = (
     <div className='wrapper'>
+        <Helmet>
+            <meta charSet="utf-8" />
+            <title>Mappening Internal Site</title>
+        </Helmet>
         <h1>Mappening Internal Site</h1>
         <Home />
     </div>
